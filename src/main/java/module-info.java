69c244124701c0d5fad391cbdd9c39b2b43f6a61
Luís Silva <ifaf.silva12@gmail.com>
2022-12-05 23:56:56 +0000

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module org.example {
     //needed for JSON
 
     requires java.sql;
+    requires json;
     opens org.example.models.FileResponse to gson;
     opens org.example to javafx.fxml;
     exports org.example;
--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -2,32 +2,64 @@ package org.example;
 
 import java.io.IOException;
 
-import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import javafx.fxml.FXML;
-import org.example.models.FileResponse.FileResponse;
+import javafx.scene.control.TextField;
+import org.json.JSONArray;
 
 public class PrimaryController {
+    HttpResponse<JsonNode> response;
+
+    @FXML
+    TextField usernameInp, passwordInp;
 
     @FXML
     private void switchToSecondary() throws IOException {
 
 
-        App.setRoot("secondary");
+
         getRandomImageAsStringFromAPI();
+        response = loginUser(usernameInp.getText(), passwordInp.getText());
+        if(response.getStatus() != 200){
+            System.out.println("ERROR");
+        }else{
+            System.out.println("sucess");
+            App.setRoot("secondary");
+        }
+        System.out.println(response.getBody().toString());
 
 
     }
 
     private static String getRandomImageAsStringFromAPI(){
         try {
-            HttpResponse<JsonNode> apiResponse = Unirest.get("https://bandini.fly.dev/api/products/60").asJson();
-            FileResponse fileResponse = new Gson().fromJson(apiResponse.getBody().toString(), FileResponse.class);
-            System.out.println(fileResponse.getProduct_id());
-            return fileResponse.getProduct_photo();
+
+            HttpResponse<JsonNode> apiResponse = Unirest.get("https://bandini.fly.dev/api/users").asJson();
+            //FileResponse fileResponse = new Gson().fromJson(apiResponse.getBody().toString(), FileResponse.class);
+            //System.out.println(apiResponse.getBody().toString());
+            JSONArray results = apiResponse.getBody().getArray();
+            //System.out.println(results.length());
+            for(int i = 0; i < results.length(); i++){
+                //System.out.println(results.get(i));
+            }
+            return apiResponse.getBody().toString();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static HttpResponse<JsonNode> loginUser(String email, String password){
+        try {
+
+            Unirest.setTimeouts(0, 0);
+            return Unirest.post("https://projetosd.fly.dev/api/login")
+                    .field("password", password)
+                    .field("email", email)
+                    .asJson();
         } catch (UnirestException e) {
             e.printStackTrace();
         }
